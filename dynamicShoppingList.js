4b4b2f985fa899950bf421bfc8d953fe3b90399b
manarzakria98 <manarzakriaabdelhameid@gmail.com>
2022-05-18 17:42:25 +0200

    initial commit

---
+++ dynamicShoppingList.js
@@ -0,0 +1,40 @@
+
+var list = document.querySelector('ul');
+var button = document.querySelector('button');
+var input = document.querySelector('input');
+
+// Enter Handler
+document.querySelector('input') 
+ .addEventListener("keyup", function(event){
+    if(event.keyCode === 13) {                                             // Enter code
+       button.click();
+      }
+   })
+     
+button.addEventListener("click", myListItem());                            // addItem Button
+
+
+function myListItem(){
+
+   var letters = /^[A-Za-z]+$/;                                          //letters only
+   if(input.value.match(letters)){
+        let myItem = input.value;
+        input.value = " ";
+
+     
+      const listItem = document.createElement('li');                           //create li dynamic
+      const listText = document.createElement('span');                         // create span
+      const listBtn = document.createElement('button');                        // create delete button
+
+      listItem.appendChild(listText);
+      listText.textContent = myItem;
+      listItem.appendChild(listBtn);
+      listBtn.textContent = 'Delete';
+      list.appendChild(listItem);
+
+      listBtn.addEventListener("click", () =>{
+            list.removeChild(listItem); 
+        });
+
+      input.focus();
+}}
